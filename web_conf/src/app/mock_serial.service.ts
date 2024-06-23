e6dff7fc0e995f4648c464dc1939fa4ace8b00d0
Naoki Iwasaki <niwasaki@f13g.com>
2024-06-24 00:03:22 +0900

    MockSerialService

---
+++ web_conf/src/app/mock_serial.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { SerialServiceInterface } from './serial.service';
+
+import { BehaviorSubject, Observable, Subject, defer, filter } from 'rxjs';
+import { AnalogSwitchAssignment, AnalogSwitchGroup, UdongConfig } from './config';
+
+@Injectable()
+export class MockSerialService extends SerialServiceInterface {
+    private message_subject = new Subject<[string, string]>();
+    private connection_subject = new BehaviorSubject<boolean>(false);
+
+    public ConnectionChanges(): Observable<boolean> {
+        return this.connection_subject;
+    }
+
+    public MessageArrives(): Observable<[string, string]> {
+        return this.message_subject;
+    }
+
+    async Connect() {
+        this.connection_subject.next(true);
+    }
+
+    async Send(message: string) {
+        console.log('send via MockSerialService', message);
+        if (message == 'get-config') {
+            this.HandleGetConfig();
+        }
+
+        return;
+    }
+
+    private MockResponse(type: string, obj: Object) {
+        this.message_subject.next([type, JSON.stringify(obj)]);
+    }
+
+    private HandleGetConfig() {
+        let assignments: AnalogSwitchAssignment[] = [];
+        for (let i = 0; i < 16; i++) {
+            let group_id = i < 8 ? 0 : 4;
+            assignments.push(
+                {
+                    analog_switch_id: i,
+                    analog_switch_group_id: group_id
+                });
+        };
+
+        let groups: AnalogSwitchGroup[] = [];
+        for (let i = 0; i < 8; i++) {
+            let group: AnalogSwitchGroup;
+            if (i < 4) {
+                group = {
+                    analog_switch_group_id: i,
+                    trigger_type: 'rapid-trigger',
+                    rapid_trigger: {
+                        act: 1.2, rel: 0.4,
+                        f_act: 3.0, f_rel: 0.2,
+                    }
+                }
+            } else {
+                group = {
+                    analog_switch_group_id: i,
+                    trigger_type: 'static-trigger',
+                    static_trigger: {
+                        act: 1.2, rel: 0.8,
+                    }
+                }
+            }
+            groups.push(group);
+        };
+
+        let udong_config: UdongConfig = {
+            analog_switch_assignments: assignments,
+            analog_switch_groups: groups
+        };
+
+        // Mock response
+        this.MockResponse('get-config', udong_config);
+    }
+}
