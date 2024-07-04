f8b0fcbf42a73b51dbdcaa05462a813dda61083b
Naoki Iwasaki <niwasaki@f13g.com>
2024-07-04 18:19:04 +0900

    .

--- firmware/src/config/config_util.cc
+++ firmware/src/config/config_util.cc
@@ -10,47 +10,25 @@ const std::vector<ButtonId>& getAllButtonIds() {
   }
 
   for (uint32_t i = 0; i < 16; i++) {
-    allButtonIds.push_back(
-        {.type = ButtonType_PUSH,
-         .which_selector = ButtonId_push_button_tag,
-         .selector{.push_button{
-             .push_button_id = i,
-         }}});
+    ButtonId button_id;
+    button_id.set_type(ButtonType::PUSH);
+    button_id.mutable_push_button()->set_push_button_id(i);
+    allButtonIds.push_back(button_id);
   }
 
-  allButtonIds.push_back(
-      {.type = ButtonType_D_PAD,
-       .which_selector = ButtonId_d_pad_tag,
-       .selector{
-           .d_pad = DPadButtonSelector{
-               .direction = DPadButtonSelector_Direction_UP,
-           }}});
+  const DPadButtonSelector::Direction kDirections[] = {
+      DPadButtonSelector::Direction::UP,
+      DPadButtonSelector::Direction::RIGHT,
+      DPadButtonSelector::Direction::DOWN,
+      DPadButtonSelector::Direction::LEFT,
+  };
 
-  allButtonIds.push_back(
-      {.type = ButtonType_D_PAD,
-       .which_selector = ButtonId_d_pad_tag,
-
-       .selector{
-           .d_pad = DPadButtonSelector{
-               .direction = DPadButtonSelector_Direction_RIGHT,
-           }}});
-
-  allButtonIds.push_back(
-      {.type = ButtonType_D_PAD,
-       .which_selector = ButtonId_d_pad_tag,
-
-       .selector{
-           .d_pad = DPadButtonSelector{
-               .direction = DPadButtonSelector_Direction_DOWN,
-           }}});
-
-  allButtonIds.push_back(
-      {.type = ButtonType_D_PAD,
-       .which_selector = ButtonId_d_pad_tag,
-       .selector{
-           .d_pad = DPadButtonSelector{
-               .direction = DPadButtonSelector_Direction_LEFT,
-           }}});
+  for (DPadButtonSelector::Direction direction : kDirections) {
+    ButtonId dpad_button;
+    dpad_button.set_type(ButtonType::D_PAD);
+    dpad_button.mutable_d_pad()->set_direction(direction);
+    allButtonIds.push_back(dpad_button);
+  }
 
   return allButtonIds;
 }
--- firmware/src/config/config_util.h
+++ firmware/src/config/config_util.h
@@ -2,49 +2,42 @@
 #define CONFIG_UTIL_H
 
 #include <Arduino.h>
-#include <ArduinoJson.h>
-
-// TODO: Get rid of Nanopb which doesn't support getter/setter
-// As a result, the runtime doesn't know whether each field is set or not.
-// So we need to maintain has_* fields manually. It is terrible for maintenance.
-#include <pb_decode.h>
-#include <pb_encode.h>
+// #include <ArduinoJson.h>
+#include <LittleFS.h>
 
 #include "../proto/config.pb.h"
-#include "json_file.h"
+// #include "json_file.h"
 
 const String kUdongConfigPath = "/user/config.binpb";
 
 inline const AnalogSwitchGroup& getConfigFromGroupId(
     uint8_t analog_switch_group_id, const UdongConfig& config) {
-  for (int i = 0; i < config.analog_switch_groups_count; i++) {
-    const AnalogSwitchGroup& group = config.analog_switch_groups[i];
-    if (group.analog_switch_group_id == analog_switch_group_id) {
+  for (const AnalogSwitchGroup& group : config.analog_switch_groups()) {
+    if (group.analog_switch_group_id() == analog_switch_group_id) {
       return group;
     }
   }
 
   // Not found.
   // TODO: Choose a solid/better solution
-  return config.analog_switch_groups[0];
+  return config.analog_switch_groups()[0];
 }
 
 inline const AnalogSwitchGroup& getConfigFromSwitchId(
     uint8_t analog_switch_id, const UdongConfig& config) {
-  for (int i = 0; i < config.analog_switch_assignments_count; i++) {
-    const AnalogSwitchAssignment& assignment =
-        config.analog_switch_assignments[i];
-    if (assignment.analog_switch_id == analog_switch_id) {
-      return getConfigFromGroupId(assignment.analog_switch_group_id, config);
+  for (const AnalogSwitchAssignment& assignment :
+       config.analog_switch_assignments()) {
+    if (assignment.analog_switch_id() == analog_switch_id) {
+      return getConfigFromGroupId(assignment.analog_switch_group_id(), config);
     }
   }
-  return config.analog_switch_groups[0];
+  return config.analog_switch_groups()[0];
 }
 
 const std::vector<ButtonId>& getAllButtonIds();
 
 inline UdongConfig defaultUdongConfig() {
-  UdongConfig config = UdongConfig_init_zero;
+  UdongConfig config;
   for (int i = 0; i < 16; i++) {
     uint8_t group_id;
     if (i >= 12 && i <= 15) {
@@ -57,45 +50,39 @@ inline UdongConfig defaultUdongConfig() {
       group_id = 4;
     }
 
-    AnalogSwitchAssignment& assignment = config.analog_switch_assignments[i];
-    assignment.analog_switch_id = i;
-    assignment.analog_switch_group_id = group_id;
+    AnalogSwitchAssignment* assignment = config.add_analog_switch_assignments();
+    assignment->set_analog_switch_id(i);
+    assignment->set_analog_switch_group_id(group_id);
   }
-  config.analog_switch_assignments_count = 16;
 
   for (int i = 0; i < 8; i++) {
-    AnalogSwitchGroup& group = config.analog_switch_groups[i];
-    group.analog_switch_group_id = i;
-    group.trigger_type =
-        i < 4 ? TriggerType_RAPID_TRIGGER : TriggerType_STATIC_TRIGGER;
-    group.has_static_trigger = true;
-    group.has_rapid_trigger = true;
-    group.static_trigger = {
-        .act = 1.2,
-        .rel = 0.8,
-    };
-    group.rapid_trigger = {
-        .act = 0.8,
-        .rel = 0.4,
-        .f_act = 3.8,
-        .f_rel = 0.2,
-    };
+    AnalogSwitchGroup& group = *config.add_analog_switch_groups();
+    group.set_analog_switch_group_id(i);
+    group.set_trigger_type(
+        i < 4 ? TriggerType::RAPID_TRIGGER : TriggerType::STATIC_TRIGGER);
+    // group->has_static_trigger = true;
+    // group.has_rapid_trigger = true;
+    group.mutable_static_trigger()->set_act(1.2);
+    group.mutable_static_trigger()->set_rel(0.8);
+    group.mutable_rapid_trigger()->set_act(0.8);
+    group.mutable_rapid_trigger()->set_rel(0.4);
+    group.mutable_rapid_trigger()->set_f_act(3.8);
+    group.mutable_rapid_trigger()->set_f_act(0.2);
   }
-  config.analog_switch_groups_count = 8;
 
   // TODO: Assign human friendly button like assigning Down button to Switch-12
   const std::vector<ButtonId>& button_ids = getAllButtonIds();
   for (int i = 0; i < 16; i++) {
-    ButtonAssignment& button_assignment = config.button_assignments[i];
-    button_assignment.switch_id = i;
-    button_assignment.has_button_id = true;
-    button_assignment.button_id = button_ids[i];
+    ButtonAssignment& button_assignment = *config.add_button_assignments();
+    button_assignment.set_switch_id(i);
+    // button_assignment.has_button_id = true;
+    *button_assignment.mutable_button_id() = button_ids[i];
   }
-  config.button_assignments_count = 16;
 
   return config;
 }
 
+/*
 inline bool write_file_callback(
     pb_ostream_t* outs, const pb_byte_t* buf, size_t count) {
   Stream* stream = (Stream*)(outs->state);
@@ -118,6 +105,7 @@ inline bool write_vector_callback(
 inline pb_ostream_s pb_ovstream(std::vector<uint8_t>& buf) {
   return {write_vector_callback, &buf, SIZE_MAX, 0};
 }
+*/
 
 inline bool SaveProtoBin(
     const String& path, const pb_msgdesc_t& fields, const void* msg) {
@@ -134,6 +122,7 @@ inline bool SaveProtoBin(
   return true;
 }
 
+/*
 inline static bool read_callback(
     pb_istream_t* ins, pb_byte_t* buf, size_t count) {
   Stream* stream = (Stream*)ins->state;
@@ -148,54 +137,49 @@ inline pb_istream_s pb_ifstream(File& file) {
 inline pb_istream_s pb_isstream(Stream& stream, size_t size) {
   return {read_callback, &stream, size, 0};
 };
+*/
 
 inline bool LoadProtoBin(
-    const String& path, const pb_msgdesc_t* fields, void* dst) {
+    const String& path, const decaproto::Message* fields, void* dst) {
   File file = LittleFS.open(path, "r");
   if (!file) {
     Serial.printf("file doesn't exist: %s\n", path.c_str());
     return false;
   }
 
-  pb_istream_s ins = pb_ifstream(file);
-  bool dec_result = pb_decode(&ins, fields, dst);
-  Serial.printf("dec_result: %d\n", dec_result);
+  // file
+  //  pb_istream_s ins = pb_ifstream(file);
+  //  bool dec_result = pb_decode(&ins, fields, dst);
+
+  // Serial.printf("dec_result: %d\n", dec_result);
 
   return true;
 }
 
 template <typename T>
-inline void complementArray(
-    const T src[], pb_size_t src_count, T dst[], pb_size_t& dst_count) {
-  while (dst_count < src_count) {
-    dst[dst_count] = src[dst_count];
-    dst_count++;
+inline void complementArray(const std::vector<T>& src, std::vector<T>* dst) {
+  dst.resize(src.size());
+  for (size_t i = 0; i < src.size(); i++) {
+    *dst[i] = src[i];
   }
 }
 
+// TODO: Implement decaproto::Message::MergeFrom and use it here
 inline void complementWithDefaultValues(UdongConfig& config) {
   UdongConfig def = defaultUdongConfig();
   complementArray(
-      def.analog_switch_assignments,
-      def.analog_switch_assignments_count,
-      config.analog_switch_assignments,
-      config.analog_switch_assignments_count);
+      def.analog_switch_assignments(),
+      config.mutable_analog_switch_assignments());
   complementArray(
-      def.analog_switch_groups,
-      def.analog_switch_groups_count,
-      config.analog_switch_groups,
-      config.analog_switch_groups_count);
+      def.analog_switch_groups(), config.mutable_analog_switch_groups());
   complementArray(
-      def.button_assignments,
-      def.button_assignments_count,
-      config.button_assignments,
-      config.button_assignments_count);
+      def.button_assignments(), config.mutable_button_assignments());
 }
 
 inline UdongConfig loadUdonConfig() {
   Serial.println("Load UdongConfig");
 
-  UdongConfig config = UdongConfig_init_zero;
+  UdongConfig config;
   if (!LoadProtoBin(kUdongConfigPath, &UdongConfig_msg, &config)) {
     Serial.println("Failed to load saved UdongConfig. Use default one.");
     return defaultUdongConfig();
