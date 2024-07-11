26e270afdb7844b64e79ee3d7d27aabd6afb7ff8
Naoki Iwasaki <niwasaki@f13g.com>
2024-07-11 20:17:32 +0900

    Board config

---
+++ firmware/custom_hwids.py
@@ -0,0 +1,14 @@
+Import("env")
+
+board_config = env.BoardConfig()
+
+USB_VID = "0x16c0"
+USB_PID = "0x27dc"
+
+board_config.update("build.arduino.earlephilhower.usb_vid", USB_VID)
+board_config.update("build.arduino.earlephilhower.usb_pid", USB_PID)
+
+# should be array of VID:PID pairs
+board_config.update("build.hwids", [
+  [USB_VID, USB_PID],
+])
